22a670fb0086290ae557e40a769ca59b485b1ba5
Grzegorz Cichosz <grzegorz.cichosz@makingwaves.com>
2017-09-18 21:47:06 +0200

    Add fail switch to dotnet release command
    Because dotnet release command is wrapped in a script it's exit code
    isn't propagated to CI. Checking dotnet release exit code, and if
    it's other than 0, returning 1 from a script should fix the issue.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 dotnet publish ./src/SlowSlothBudget.Web -c Release -o out
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
 cp -r ./src/SlowSlothBudget.Web/out ./out
 docker info
 docker build -t slowsloth/slowslothbudget-web .
